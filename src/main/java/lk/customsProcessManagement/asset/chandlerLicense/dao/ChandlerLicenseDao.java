b5bd8a9b85409210f778f7ba69efbf6ab7aeb3af
LalithK90 <asakahatapitiya@gmail.com>
2020-09-29 14:21:05 +0530

    Last saved chandler license license number take method was created on ChandlerLicenseDao

--- src/main/java/lk/customsProcessManagement/asset/chandlerLicense/dao/ChandlerLicenseDao.java
+++ src/main/java/lk/customsProcessManagement/asset/chandlerLicense/dao/ChandlerLicenseDao.java
@@ -10,4 +10,6 @@ import java.util.List;
 @Repository
 public interface ChandlerLicenseDao extends JpaRepository< ChandlerLicense, Integer > {
     List< ChandlerLicense> findByLicenseStatus(LicenseStatus licenseStatus);
+
+    ChandlerLicense findFirstByOrderByIdDesc();
 }
