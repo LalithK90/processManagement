d63c0e5f8d6286f8db63e7f7568c1dc62c14e231
LalithK90 <asakahatapitiya@gmail.com>
2020-10-17 00:19:05 +0530

    Payment was created becouse of the had to change VezzalOrderItemApproved

---
+++ src/main/java/lk/customsProcessManagement/asset/vezzalOrderItemApproved/entity/VezzalOrderItemApproved.java
@@ -0,0 +1,48 @@
+package lk.customsProcessManagement.asset.vezzalOrderItemApproved.entity;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import lk.customsProcessManagement.asset.chandler.entity.Chandler;
+import lk.customsProcessManagement.asset.payment.entity.Payment;
+import lk.customsProcessManagement.asset.shipAgent.entity.ShipAgent;
+import lk.customsProcessManagement.asset.vezzalOrder.entity.VezzalOrder;
+import lk.customsProcessManagement.util.audit.AuditEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonFilter( "VezzalOrderItemApproved" )
+
+public class VezzalOrderItemApproved extends AuditEntity {
+  @NotEmpty
+  @Column( nullable = false )
+  private String itemCount;
+
+  @Column( nullable = false, precision = 10, scale = 2 )
+  private BigDecimal amount;
+
+  @ManyToOne
+  private VezzalOrder vezzalOrder;
+
+  @ManyToOne
+  private Chandler chandler;
+
+  @ManyToOne
+  private ShipAgent shipAgent;
+
+  @OneToMany(mappedBy = "vezzalOrderItemApproved")
+  private List< Payment > payments;
+}
