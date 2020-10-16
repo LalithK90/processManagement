2f0199308cbf913e0b2f725fdb088c28b6a35ed3
LalithK90 <asakahatapitiya@gmail.com>
2020-10-17 00:03:15 +0530

    BitValidOrNot was created according to that VezzalOrderItem was modified

--- src/main/java/lk/customsProcessManagement/asset/vezzalOrderItem/entity/VezzalOrderItem.java
+++ src/main/java/lk/customsProcessManagement/asset/vezzalOrderItem/entity/VezzalOrderItem.java
@@ -3,16 +3,17 @@ package lk.customsProcessManagement.asset.vezzalOrderItem.entity;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import lk.customsProcessManagement.asset.item.entity.Item;
 import lk.customsProcessManagement.asset.vezzalOrder.entity.VezzalOrder;
+import lk.customsProcessManagement.asset.vezzalOrderItem.entity.Enum.VezzalOrderItemStatus;
+import lk.customsProcessManagement.asset.vezzalOrderItemBit.entity.VezzalOrderItemBit;
 import lk.customsProcessManagement.util.audit.AuditEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
 
 @Entity
 @Getter
@@ -23,12 +24,18 @@ import javax.validation.constraints.NotEmpty;
 public class VezzalOrderItem extends AuditEntity {
 
   @NotEmpty
-  @Column(nullable = false)
+  @Column( nullable = false )
   private String quantity;
+
+  @Enumerated( EnumType.STRING )
+  private VezzalOrderItemStatus vezzalOrderItemStatus;
 
   @ManyToOne
   private Item item;
 
   @ManyToOne
   private VezzalOrder vezzalOrder;
+
+  @OneToMany( mappedBy = "vezzalOrderItem" )
+  private List< VezzalOrderItemBit > vezzalOrderItemBit;
 }
