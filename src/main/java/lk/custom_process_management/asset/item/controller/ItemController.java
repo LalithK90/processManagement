b18e26259cac022166235eb9f0a377770867e432
LalithK90 <asakahatapitiya@gmail.com>
2020-10-23 11:10:14 +0530

    Item module was updated

--- src/main/java/lk/custom_process_management/asset/item/controller/ItemController.java
+++ src/main/java/lk/custom_process_management/asset/item/controller/ItemController.java
@@ -1,7 +1,6 @@
 package lk.custom_process_management.asset.item.controller;
 
 
-
 import lk.custom_process_management.asset.category.controller.CategoryController;
 import lk.custom_process_management.asset.item.entity.Enum.ItemStatus;
 import lk.custom_process_management.asset.item.entity.Enum.MainCategory;
@@ -20,71 +19,74 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/item")
-public class ItemController implements AbstractController< Item, Integer> {
-    private final ItemService itemService;
-    private final MakeAutoGenerateNumberService makeAutoGenerateNumberService;
+@RequestMapping( "/item" )
+public class ItemController implements AbstractController< Item, Integer > {
+  private final ItemService itemService;
+  private final MakeAutoGenerateNumberService makeAutoGenerateNumberService;
 
-    @Autowired
-    public ItemController(ItemService itemService, MakeAutoGenerateNumberService makeAutoGenerateNumberService) {
-        this.itemService = itemService;
-        this.makeAutoGenerateNumberService = makeAutoGenerateNumberService;
+  @Autowired
+  public ItemController(ItemService itemService, MakeAutoGenerateNumberService makeAutoGenerateNumberService) {
+    this.itemService = itemService;
+    this.makeAutoGenerateNumberService = makeAutoGenerateNumberService;
+  }
+
+  private String commonThings(Model model, Item item, Boolean addState) {
+    model.addAttribute("itemStatuses", ItemStatus.values());
+    model.addAttribute("item", item);
+    model.addAttribute("addStatus", addState);
+    model.addAttribute("mainCategories", MainCategory.values());
+    model.addAttribute("urlMainCategory", MvcUriComponentsBuilder
+        .fromMethodName(CategoryController.class, "getCategoryByMainCategory", "")
+        .build()
+        .toString());
+    return "item/addItem";
+  }
+
+  @GetMapping
+  public String findAll(Model model) {
+    model.addAttribute("items", itemService.findAll());
+    return "item/item";
+  }
+
+  @GetMapping( "/add" )
+  public String addForm(Model model) {
+    return commonThings(model, new Item(), true);
+  }
+
+  @PostMapping( value = {"/save", "/update"} )
+  public String persist(@Valid @ModelAttribute Item item, BindingResult bindingResult,
+                        RedirectAttributes redirectAttributes, Model model) {
+    if ( bindingResult.hasErrors() ) {
+      return commonThings(model, item, true);
     }
 
-    private String commonThings(Model model, Item item, Boolean addState) {
-        model.addAttribute("itemStatuses", ItemStatus.values());
-        model.addAttribute("item", item);
-        model.addAttribute("addStatus", addState);
-        model.addAttribute("mainCategories", MainCategory.values());
-        model.addAttribute("urlMainCategory", MvcUriComponentsBuilder
-                .fromMethodName(CategoryController.class, "getCategoryByMainCategory", "")
-                .build()
-                .toString());
-        return "item/addItem";
-    }
+    itemService.persist(item);
+    return "redirect:/item";
+  }
 
-    @GetMapping
-    public String findAll(Model model) {
-        model.addAttribute("items", itemService.findAll());
-        return "item/item";
-    }
+  @GetMapping( "/edit/{id}" )
+  public String edit(@PathVariable Integer id, Model model) {
+    return commonThings(model, itemService.findById(id), false);
+  }
 
-    @GetMapping("/add")
-    public String addForm(Model model) {
-        return commonThings(model, new Item(), true);
-    }
+  @GetMapping( "/delete/{id}" )
+  public String delete(@PathVariable Integer id, Model model) {
+    itemService.delete(id);
+    return "redirect:/item";
+  }
 
-    @PostMapping(value = {"/save", "/update"})
-    public String persist(@Valid @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
-        if (bindingResult.hasErrors()) {
-            return commonThings(model, item, true);
-        }
+  @GetMapping( "/{id}" )
+  public String view(@PathVariable Integer id, Model model) {
+    model.addAttribute("itemDetail", itemService.findById(id));
+    return "item/item-detail";
+  }
 
-        itemService.persist(item);
-        return "redirect:/item";
-    }
-
-    @GetMapping("/edit/{id}")
-    public String edit(@PathVariable Integer id, Model model) {
-        return commonThings(model, itemService.findById(id), false);
-    }
-
-    @GetMapping("/delete/{id}")
-    public String delete(@PathVariable Integer id, Model model) {
-        itemService.delete(id);
-        return "redirect:/item";
-    }
-
-    @GetMapping("/{id}")
-    public String view(@PathVariable Integer id, Model model) {
-        model.addAttribute("itemDetail", itemService.findById(id));
-        return "item/item-detail";
-    }
-
-    @GetMapping("/itemBatch/{id}")
-    public String itemBatch(@PathVariable Integer id, Model model) {
-        model.addAttribute("itemDetail", itemService.findById(id));
-        return "itemBatch/addItemBatch";
-    }
+  @GetMapping( "/deactivate/{id}" )
+  public String itemDeactivate(@PathVariable Integer id) {
+    Item item = itemService.findById(id);
+    item.setItemStatus(ItemStatus.STOP);
+    itemService.persist(item);
+    return "redirect:/item";
+  }
 
 }
--- src/main/java/lk/custom_process_management/asset/item/service/ItemService.java
+++ src/main/java/lk/custom_process_management/asset/item/service/ItemService.java
@@ -11,81 +11,80 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
-import javax.management.loading.MLetContent;
 import java.util.List;
 
 @Service
-@CacheConfig(cacheNames = "item")
-public class ItemService implements AbstractService< Item, Integer> {
-    private final ItemDao itemDao;
+@CacheConfig( cacheNames = "item" )
+public class ItemService implements AbstractService< Item, Integer > {
+  private final ItemDao itemDao;
 
-    @Autowired
-    public ItemService(ItemDao itemDao) {
-        this.itemDao = itemDao;
+  @Autowired
+  public ItemService(ItemDao itemDao) {
+    this.itemDao = itemDao;
+  }
+
+  public List< Item > findAll() {
+    return itemDao.findAll();
+  }
+
+  public Item findById(Integer id) {
+    return itemDao.getOne(id);
+  }
+
+  private String makeItemCode(String lastNumber) {
+    String newNumber = "";
+    if ( lastNumber != null ) {
+      int number = Integer.parseInt(lastNumber);
+
+      if ( number < 10 ) {
+        newNumber = "00" + (number + 1);
+      }
+      if ( 10 < number && number < 100 ) {
+        newNumber = "0" + (number + 1);
+      }
+      if ( 100 < number ) {
+        newNumber = String.valueOf(number + 1);
+      }
+      return newNumber;
+    } else {
+      return "0001";
+
     }
+  }
 
-    public List<Item> findAll() {
-        return itemDao.findAll();
+  public Item persist(Item item) {
+    if ( item.getId() == null ) {
+      //need to create code to item
+      String code = item.getCategory().getMainCategory()
+          + item.getCategory().getName().trim().substring(0, 2)
+          + item.getName().trim().substring(0, 2);
+      //check last item on db
+      Item itemDB = itemDao.findFirstByOrderByIdDesc();
+      if ( itemDB != null ) {
+        item.setCode(code + makeItemCode(itemDB.getCode().substring(6)));
+      } else {
+        item.setCode(code + makeItemCode(null));
+      }
+      item.setItemStatus(ItemStatus.NOT_AVAILABLE);
     }
+    return itemDao.save(item);
+  }
 
-    public Item findById(Integer id) {
-        return itemDao.getOne(id);
-    }
+  public boolean delete(Integer id) {
+    itemDao.deleteById(id);
+    return false;
+  }
 
-    private String makeItemCode(String lastNumber){
-        String newNumber = "";
-        if (lastNumber !=null) {
-            int number = Integer.parseInt(lastNumber);
+  public List< Item > search(Item item) {
+    ExampleMatcher matcher = ExampleMatcher
+        .matching()
+        .withIgnoreCase()
+        .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+    Example< Item > itemExample = Example.of(item, matcher);
+    return itemDao.findAll(itemExample);
+  }
 
-            if (number<10){
-                newNumber = "00"+ (number + 1);
-            }
-            if (10 < number && number < 100){
-                newNumber = "0"+ (number + 1);
-            }
-            if (100<number){
-                newNumber = String.valueOf(number+1);
-            }
-            return newNumber;
-        }else {
-            return  "0001";
-
-        }
-    }
-
-    public Item persist(Item item) {
-        if (item.getId() == null) {
-            //need to create code to item
-            String code =item.getCategory().getMainCategory()
-                    + item.getCategory().getName().trim().substring(0,2)
-                    + item.getName().trim().substring(0,2);
-            //check last item on db
-            Item itemDB = itemDao.findFirstByOrderByIdDesc();
-            if ( itemDB != null ){
-                item.setCode(code +makeItemCode(itemDB.getCode().substring(6)));
-            }else{
-                      item.setCode(code +makeItemCode(null));
-            }
-            item.setItemStatus(ItemStatus.NOT_AVAILABLE);
-        }
-        return itemDao.save(item);
-    }
-
-    public boolean delete(Integer id) {
-        itemDao.deleteById(id);
-        return false;
-    }
-
-    public List<Item> search(Item item) {
-        ExampleMatcher matcher = ExampleMatcher
-                .matching()
-                .withIgnoreCase()
-                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
-        Example<Item> itemExample = Example.of(item, matcher);
-        return itemDao.findAll(itemExample);
-    }
-
-    public Item lastItem() {
-        return itemDao.findFirstByOrderByIdDesc();
-    }
+  public Item lastItem() {
+    return itemDao.findFirstByOrderByIdDesc();
+  }
 }
