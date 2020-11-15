4c756d5e62524cb9e5ceb325d23f53242d674780
LalithK90 <asakahatapitiya@gmail.com>
2020-11-16 00:22:02 +0530

    Profile Controller was modified

--- src/main/java/lk/custom_process_management/asset/common_asset/controller/ProfileController.java
+++ src/main/java/lk/custom_process_management/asset/common_asset/controller/ProfileController.java
@@ -1,5 +1,7 @@
 package lk.custom_process_management.asset.common_asset.controller;
 
+import lk.custom_process_management.asset.user_details.entity.UserDetails;
+import lk.custom_process_management.asset.user_details.service.UserDetailsFilesService;
 import lk.custom_process_management.asset.user_management.entity.PasswordChange;
 import lk.custom_process_management.asset.user_management.entity.User;
 import lk.custom_process_management.asset.user_management.service.UserService;
@@ -21,18 +23,22 @@ import java.security.Principal;
 public class ProfileController {
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final UserDetailsFilesService userDetailsFilesService;
 
     @Autowired
-    public ProfileController(UserService userService, PasswordEncoder passwordEncoder) {
+    public ProfileController(UserService userService, PasswordEncoder passwordEncoder,
+                             UserDetailsFilesService userDetailsFilesService) {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
+        this.userDetailsFilesService = userDetailsFilesService;
     }
 
     @GetMapping( value = "/profile" )
     public String userProfile(Model model, Principal principal) {
-        model.addAttribute("addStatus", true);
-        model.addAttribute("employeeDetail", userService.findByUserName(principal.getName()).getUserDetails());
-        return "employee/employee-detail";
+        UserDetails userDetails = userService.findByUserName(principal.getName()).getUserDetails();
+        model.addAttribute("userDetail", userDetails);
+        model.addAttribute("file", userDetailsFilesService.userDetailsFileDownloadLinks(userDetails));
+        return "userDetails/userDetails-detail";
     }
 
     @GetMapping( value = "/passwordChange" )
