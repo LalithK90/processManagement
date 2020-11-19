c06bac88e04cd5a059a2ee087d7f4ae219c83d22
LalithK90 <asakahatapitiya@gmail.com>
2020-11-19 18:30:57 +0530

    Vezzal Order Status Change SSchedule was partially completed.

---
+++ src/main/java/lk/custom_process_management/schedule/VezzalOrderStatusChange.java
@@ -0,0 +1,6 @@
+package lk.custom_process_management.schedule;
+
+public class VezzalOrderStatusChange {
+  //if vezzal order closing date would
+  // be exceeded need to be changed it status to Vezzal Order Status to invalid
+}
