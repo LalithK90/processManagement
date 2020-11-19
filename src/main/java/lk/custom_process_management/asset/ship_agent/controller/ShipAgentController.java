f402c8cc160c60fcc76860822a5ef7f12f5d9592
LalithK90 <asakahatapitiya@gmail.com>
2020-11-19 19:53:34 +0530

    Ship agent findAll method was created

--- src/main/java/lk/custom_process_management/asset/ship_agent/controller/ShipAgentController.java
+++ src/main/java/lk/custom_process_management/asset/ship_agent/controller/ShipAgentController.java
@@ -11,69 +11,84 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
-@RequestMapping("/shipAgent")
+@RequestMapping( "/shipAgent" )
 public class ShipAgentController implements AbstractController< ShipAgent, Integer > {
-private final ShipAgentService shipAgentService;
-    private final MakeAutoGenerateNumberService makeAutoGenerateNumberService;
+  private final ShipAgentService shipAgentService;
+  private final MakeAutoGenerateNumberService makeAutoGenerateNumberService;
 
-    public ShipAgentController(ShipAgentService shipAgentService,
-                               MakeAutoGenerateNumberService makeAutoGenerateNumberService) {
-        this.shipAgentService = shipAgentService;
-        this.makeAutoGenerateNumberService = makeAutoGenerateNumberService;
-    }
-    private String commonThings(Model model, ShipAgent shipAgent, Boolean addState) {
-        model.addAttribute("shipAgent", shipAgent);
-        model.addAttribute("addStatus", addState);
-        return "shipAgent/addShipAgent";
-    }
+  public ShipAgentController(ShipAgentService shipAgentService,
+                             MakeAutoGenerateNumberService makeAutoGenerateNumberService) {
+    this.shipAgentService = shipAgentService;
+    this.makeAutoGenerateNumberService = makeAutoGenerateNumberService;
+  }
 
-    @GetMapping
-    public String findAll(Model model) {
-        model.addAttribute("shipAgents", shipAgentService.findAll());
-        return "shipAgent/shipAgent";
-    }
-@GetMapping("/add")
-    public String addForm(Model model) {
+  private String commonThings(Model model, ShipAgent shipAgent, Boolean addState) {
+    model.addAttribute("shipAgent", shipAgent);
+    model.addAttribute("addStatus", addState);
+    return "shipAgent/addShipAgent";
+  }
+
+  @GetMapping
+  public String findAll(Model model) {
+    model.addAttribute("shipAgents", shipAgentService.findAll());
+    return "shipAgent/shipAgent";
+  }
+
+  @GetMapping( "/add" )
+  public String addForm(Model model) {
     return commonThings(model, new ShipAgent(), true);
-}
-@PostMapping(value = {"/add","/save","/update"})
-    public String persist(@Valid @ModelAttribute ShipAgent shipAgent, BindingResult bindingResult, RedirectAttributes redirectAttributes,
-                          Model model){
-    if (bindingResult.hasErrors()) {
-        return commonThings(model, shipAgent, true);
+  }
+
+  @PostMapping( value = {"/add", "/save", "/update"} )
+  public String persist(@Valid @ModelAttribute ShipAgent shipAgent, BindingResult bindingResult,
+                        RedirectAttributes redirectAttributes,
+                        Model model) {
+    if ( bindingResult.hasErrors() ) {
+      return commonThings(model, shipAgent, true);
     }
 
 //if vezzal has id that vezzal is not a new vezzal
-    if (shipAgent.getId() == null) {
-        //if there is not vezzal in db
-        if (shipAgentService.lastShipAgent() == null) {
-            //need to generate new onecustomer
-            shipAgent.setCode("LKCSA"+makeAutoGenerateNumberService.numberAutoGen(null).toString());
-        } else {
-            //if there is vezzal in db need to get that vezzal's code and increase its value
-            String previousCode = shipAgentService.lastShipAgent().getCode().substring(5);
-            shipAgent.setCode("LKCSA"+makeAutoGenerateNumberService.numberAutoGen(previousCode).toString());
-        }
+    if ( shipAgent.getId() == null ) {
+      //if there is not vezzal in db
+      if ( shipAgentService.lastShipAgent() == null ) {
+        //need to generate new onecustomer
+        shipAgent.setCode("LKCSA" + makeAutoGenerateNumberService.numberAutoGen(null).toString());
+      } else {
+        //if there is vezzal in db need to get that vezzal's code and increase its value
+        String previousCode = shipAgentService.lastShipAgent().getCode().substring(5);
+        shipAgent.setCode("LKCSA" + makeAutoGenerateNumberService.numberAutoGen(previousCode).toString());
+      }
 
     }
 
     redirectAttributes.addFlashAttribute("shipAgentDetail", shipAgentService.persist(shipAgent));
     return "redirect:/shipAgent";
-    }
-@GetMapping("/edit/{id}")
-    public String edit(@PathVariable Integer id, Model model) {
+  }
+
+  @GetMapping( "/edit/{id}" )
+  public String edit(@PathVariable Integer id, Model model) {
     return commonThings(model, shipAgentService.findById(id), false);
-}
-@GetMapping("/delete/{id}")
-    public String delete(@PathVariable Integer id, Model model) {
+  }
+
+  @GetMapping( "/delete/{id}" )
+  public String delete(@PathVariable Integer id, Model model) {
     shipAgentService.delete(id);
     return "redirect:/shipAgent";
-    }
-@GetMapping("/view/{id}")
-    public String view(@PathVariable Integer id, Model model) {
+  }
+
+  @GetMapping( "/view/{id}" )
+  public String view(@PathVariable Integer id, Model model) {
     model.addAttribute("shipAgentDetail", shipAgentService.findById(id));
     return "shipAgent/shipAgent-detail";
-    }
+  }
+
+  @GetMapping( "/findAll" )
+  public List< ShipAgent > findAll() {
+    return shipAgentService.findAll();
+  }
+
+
 }
