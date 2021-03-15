aa91f95605d33a960210b9c5ed9460ae04876de2
LalithK90 <asakahatapitiya@gmail.com>
2021-03-16 01:44:22 +0530

    Schedule was fixed

--- src/main/java/lk/custom_process_management/schedule/VesselOrderSchedule.java
+++ src/main/java/lk/custom_process_management/schedule/VesselOrderSchedule.java
@@ -3,6 +3,8 @@ package lk.custom_process_management.schedule;
 import lk.custom_process_management.asset.vessel_order.entity.VesselOrder;
 import lk.custom_process_management.asset.vessel_order.entity.enums.VesselOrderStatus;
 import lk.custom_process_management.asset.vessel_order.service.VesselOrderService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.time.LocalDate;
@@ -10,6 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Configuration
+@EnableScheduling
 public class VesselOrderSchedule {
   private final VesselOrderService vesselOrderService;
 
@@ -17,9 +21,8 @@ public class VesselOrderSchedule {
     this.vesselOrderService = vesselOrderService;
   }
 
-  @Scheduled( cron = "0 0 * * ?" )
+  @Scheduled( cron = "0 0 0 * * *" )
   void fourthSchedule() {
-    System.out.println("sdasdjads sadjsads  jsda");
     List< VesselOrder > vesselOrders = new ArrayList<>();
     vesselOrderService.findByVesselOrderStatus(VesselOrderStatus.PROCESSING)
         .stream()
@@ -32,89 +35,5 @@ public class VesselOrderSchedule {
     vesselOrderService.saveAll(vesselOrders);
   }
 
-  //Number of milliseconds to delay before the first execution of a {@link #fixedRate()} or {@link #fixedDelay()} task.
 
-  // fixed rate give value in milisecound
-//  @Scheduled( fixedRate = 1000 )
-//  void fristSchedule() {
-//    System.out.println(new Date().toString());
-//
-//  }
-
-
-  // fixed delay give value in milisecound
-
-//  @Scheduled( fixedDelay = 1000 )
-//  void secondSchedule() {
-//    System.out.println(new Date().toString());
-//
-//  }
-
-// deference delay and fixed rate
-  //execute as specific interval even though previous execution also running
-  //but delay
-  // after complete the previous execution and wait given time duration and execute again
-
-
-  /*     * Number of milliseconds to delay before the first execution of a
-   * {@link #fixedRate()} or {@link #fixedDelay()} task.
-   * @return the initial delay in milliseconds as a String value, e.g. a placeholder
-   * or a {@link java.time.Duration#parse java.time.Duration} compliant value
-   * @since 3.2.2
-   */
-//  @Scheduled( fixedDelay = 2000, initialDelay = 4000 )
-//  void thirdSchedule() {
-//    System.out.println(new Date().toString());
-//
-//  }
-
-
-  /*
-   * An annotation that marks a method to be scheduled. Exactly one of
-   * the {@link #cron()}, {@link #fixedDelay()}, or {@link #fixedRate()}
-   * attributes must be specified.
-   */
-//  @Scheduled(cron ="{every second(*)},{every minute(*)}, {every hour(*)}, {every day(*)},{every month(*)},{every
-//  year(?)}" )
-  // above crone execute in every one second
-  //@Scheduled(cron ="* * * * * ?")
-  //@Scheduled( cron = "20 08 17 * * ?" )  // every day every month 8.08.17 pm will be execute
-//  void fourthSchedule() {
-//    System.out.println(new Date().toString());
-//
-//  }
-
-//  @Scheduled( fixedRate = 1000 )
-//  void fiveSchedule() throws InterruptedException {
-//    Thread.sleep(1000);
-//    System.out.println(new Date().toString());
-//
-//  }
-
-  /*
-   # At 12:00 pm (noon) every day:- @Schedule(0 12 * * ?)
-
-   # Every 5 minutes starting at 1 pm and ending on 1:55 pm and then starting at 6 pm and ending at 6:55 pm, every
-   day:- @Schedule(0/5 13,18 * * ?)
-
-   # Every minute starting at 1 pm and ending on 1:05 pm, every day:- @Schedule(0-5 13 * * ?)
-
-   # At 1:15 pm and 1:45 pm every Tuesday in the month of June:- @Schedule(15,45 13 ? 6 Tue)
-
-   # At 9:30 am every Monday, Tuesday, Wednesday, Thursday, and Friday:- @Schedule(30 9 ? * MON-FRI)
-
-   # At 9:30 am on 15th day of every month:- @Schedule(30 9 15 * ?)
-
-   # At 6 pm on the last day of every month:- @Schedule(0 18 L * ?)
-
-   # At 6 pm on the 3rd to last day of every month:- @Schedule(0 18 L-3 * ?)
-
-   # At 10:30 am on the last Thursday of every month:- @Schedule(30 10 ? * 5L)
-
-   # At 10 am on the third Monday of every month:- @Schedule(0 10 ? * 2#3)
-
-   # At 12 am midnight on every day for five days starting on the 10th day of the month:- @Schedule(0 0 10/5 * ?)
-
-
-   */
 }
