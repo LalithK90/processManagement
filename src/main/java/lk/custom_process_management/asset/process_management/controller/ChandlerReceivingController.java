bdfcb77bb275fd25e248dec6889414a5d85da9dc
LalithK90 <asakahatapitiya@gmail.com>
2021-04-11 00:03:10 +0530

    ChandlerReceivingController was fixed

--- src/main/java/lk/custom_process_management/asset/process_management/controller/ChandlerReceivingController.java
+++ src/main/java/lk/custom_process_management/asset/process_management/controller/ChandlerReceivingController.java
@@ -8,12 +8,14 @@ import lk.custom_process_management.util.service.DateTimeAgeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.model.IModel;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping("/chandlerReceiving")
+@RequestMapping( "/chandlerReceiving" )
 public class ChandlerReceivingController {
 
   private final PaymentService paymentService;
@@ -31,7 +33,7 @@ public class ChandlerReceivingController {
                        paymentService.findByCreatedAtIsBetween(dateTimeAgeService.dateTimeToLocalDateStartInDay(from)
                            , dateTimeAgeService.dateTimeToLocalDateEndInDay(to))
                            .stream()
-                           .filter(x -> x.getStatusConformation().equals(StatusConformation.REC))
+                           .filter(x -> x.getStatusConformation().equals(StatusConformation.RECEVINGPAYMENT))
                            .collect(Collectors.toList()));
 
     model.addAttribute("message",
@@ -58,7 +60,43 @@ public class ChandlerReceivingController {
     payment.setStatusConformation(StatusConformation.RECEVINGPAYMENT);
     model.addAttribute("chandlerDetail", payment.getChandler());
     model.addAttribute("payment", payment);
+    model.addAttribute("addStatus", true);
     return "payment/addPayment";
   }
-  //todo need to mentioned cancel one as cancel
+
+  @GetMapping( "/reject/{id}" )
+  public String addPaymentReject(@PathVariable( "id" ) Integer id, Model model) {
+    Payment payment = paymentService.findById(id);
+    payment.setStatusConformation(StatusConformation.REJECTCHA);
+    paymentService.persist(payment);
+    return "redirect:/chandlerReceiving";
+  }
+
+  @GetMapping( "/reject" )
+  public String addPaymentRejectAll(Model model) {
+    return commonReject(model, paymentService.findByStatusConformation(StatusConformation.REJECTCHA), "All Rejected " +
+        "Payment By Chandler");
+  }
+
+  @PostMapping( "/reject" )
+  public String addPaymentRejectSearch(@ModelAttribute TwoDate twoDate, Model model) {
+
+    List< Payment > payments =
+        paymentService.findByCreatedAtIsBetween(dateTimeAgeService.dateTimeToLocalDateStartInDay(twoDate.getStartDate())
+        , dateTimeAgeService.dateTimeToLocalDateEndInDay(twoDate.getEndDate()))
+        .stream()
+        .filter(x -> x.getStatusConformation().equals(StatusConformation.REJECTCHA))
+        .collect(Collectors.toList());
+
+    return commonReject(model, payments,
+                        "All Rejected Payment By Chandler since" + twoDate.getStartDate() + " to " + twoDate.getEndDate());
+  }
+
+  private String commonReject(Model model, List< Payment > payments, String message) {
+    model.addAttribute("payments", payments);
+    model.addAttribute("message", message);
+    model.addAttribute("searchUrl", "reject");
+    return "vesselOrderItemBidReceived/vesselOrderItemBidReceived";
+  }
+
 }
