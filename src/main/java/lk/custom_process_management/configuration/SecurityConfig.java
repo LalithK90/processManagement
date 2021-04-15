9e97c52ea3be76bbb9bfd8d05525ca40969698b5
LalithK90 <asakahatapitiya@gmail.com>
2021-04-15 15:37:50 +0530

    vessel report was completed

--- src/main/java/lk/custom_process_management/configuration/SecurityConfig.java
+++ src/main/java/lk/custom_process_management/configuration/SecurityConfig.java
@@ -71,11 +71,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-            http.csrf().disable();
-            http.authorizeRequests().antMatchers("/").permitAll();
+/*            http.csrf().disable();
+            http.authorizeRequests().antMatchers("/").permitAll();*/
     // For developing easy to give permission all lin
 
-  /*  http
+    http
         .authorizeRequests(
             authorizeRequests ->
                 authorizeRequests
@@ -120,7 +120,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //Cross site disable
         .csrf(AbstractHttpConfigurer::disable)
         .exceptionHandling();
-        */
   }
 }
 
