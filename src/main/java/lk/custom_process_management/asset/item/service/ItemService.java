120d8cfb4aafeb0ee77459ae07d1c8fbf75732cc
LalithK90 <asakahatapitiya@gmail.com>
2021-05-14 18:01:51 +0530

    item number generator was updated

--- src/main/java/lk/custom_process_management/asset/item/service/ItemService.java
+++ src/main/java/lk/custom_process_management/asset/item/service/ItemService.java
@@ -33,42 +33,9 @@ public class ItemService implements AbstractService< Item, Integer > {
     return itemDao.getOne(id);
   }
 
-  private String makeItemCode(String lastNumber) {
-    String newNumber = "";
-    if ( lastNumber != null ) {
-      int number = Integer.parseInt(lastNumber);
-
-      if ( number < 10 ) {
-        newNumber = "00" + (number + 1);
-      }
-      if ( 10 < number && number < 100 ) {
-        newNumber = "0" + (number + 1);
-      }
-      if ( 100 < number ) {
-        newNumber = String.valueOf(number + 1);
-      }
-      return newNumber;
-    } else {
-      return "0001";
-
-    }
-  }
-
   public Item persist(Item item) {
     if ( item.getId() == null ) {
       item.setLiveDead(LiveDead.ACTIVE);
-      //need to create code to item
-      String code = item.getCategory().getMainCategory()
-          + item.getCategory().getName().trim().substring(0, 2)
-          + item.getName().trim().substring(0, 2);
-      //check last item on db
-      Item itemDB = itemDao.findFirstByOrderByIdDesc();
-      if ( itemDB != null ) {
-        item.setCode(code + makeItemCode(itemDB.getCode().substring(6)));
-      } else {
-        item.setCode(code + makeItemCode(null));
-      }
-      item.setItemStatus(ItemStatus.NOT_AVAILABLE);
     }
     return itemDao.save(item);
   }
