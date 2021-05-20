d202e78b36b951de4fb91f8cd85fc43bd5d6ad31
Sandaruwan <ssgamagedara@gmail.com>
2021-05-21 00:25:23 +0530

    Project functions authorization

--- src/main/java/lk/custom_process_management/asset/process_management/controller/VesselOrderItemBidApprovalController.java
+++ src/main/java/lk/custom_process_management/asset/process_management/controller/VesselOrderItemBidApprovalController.java
@@ -30,144 +30,144 @@ import java.util.stream.Collectors;
 @Controller
 @RequestMapping( "/vesselOrderItemApproval" )
 public class VesselOrderItemBidApprovalController {
-  private final VesselOrderService vesselOrderService;
-  private final VesselOrderItemService vesselOrderItemService;
-  private final VesselOrderItemBidService vesselOrderItemBidService;
-  private final VesselOrderItemBidPaymentService vesselOrderItemBidPaymentService;
-  private final PaymentService paymentService;
-  private final MakeAutoGenerateNumberService makeAutoGenerateNumberService;
-  private final EmailService emailService;
+    private final VesselOrderService vesselOrderService;
+    private final VesselOrderItemService vesselOrderItemService;
+    private final VesselOrderItemBidService vesselOrderItemBidService;
+    private final VesselOrderItemBidPaymentService vesselOrderItemBidPaymentService;
+    private final PaymentService paymentService;
+    private final MakeAutoGenerateNumberService makeAutoGenerateNumberService;
+    private final EmailService emailService;
 
-  public VesselOrderItemBidApprovalController(VesselOrderService vesselOrderService,
-                                              VesselOrderItemService vesselOrderItemService,
-                                              VesselOrderItemBidService vesselOrderItemBidService,
-                                              VesselOrderItemBidPaymentService vesselOrderItemBidPaymentService,
-                                              PaymentService paymentService,
-                                              MakeAutoGenerateNumberService makeAutoGenerateNumberService,
-                                              EmailService emailService) {
-    this.vesselOrderService = vesselOrderService;
-    this.vesselOrderItemService = vesselOrderItemService;
-    this.vesselOrderItemBidService = vesselOrderItemBidService;
-    this.vesselOrderItemBidPaymentService = vesselOrderItemBidPaymentService;
-    this.paymentService = paymentService;
-    this.makeAutoGenerateNumberService = makeAutoGenerateNumberService;
-    this.emailService = emailService;
-  }
-
-
-  @GetMapping
-  public String findAll(Model model) {
-    model.addAttribute("vesselOrders", vesselOrderService.findByVesselOrderStatus(VesselOrderStatus.BIDEN));
-    return "vesselOrderItemApproval/vesselOrderItemApproval";
-  }
-
-  @GetMapping( "/{id}" )
-  public String addForm(@PathVariable Integer id, Model model) {
-    Comparator< VesselOrderItemBid > compareByBidenPrice =
-        Comparator.comparing(VesselOrderItemBid::getAmount);
-
-    List< VesselOrderItemBid > vesselOrderItemBids = new ArrayList<>();
-
-    VesselOrder vesselOrder = vesselOrderService.findById(id);
-
-    vesselOrder.getVesselOrderItems().forEach(
-        x -> vesselOrderItemBidService.findByVesselOrderItem(x)
-            .stream()
-            .sorted(compareByBidenPrice)
-            .collect(Collectors.toList()).forEach(y -> {
-              y.setItem(x.getItem());
-              vesselOrderItemBids.add(y);
-            }));
+    public VesselOrderItemBidApprovalController(VesselOrderService vesselOrderService,
+                                                VesselOrderItemService vesselOrderItemService,
+                                                VesselOrderItemBidService vesselOrderItemBidService,
+                                                VesselOrderItemBidPaymentService vesselOrderItemBidPaymentService,
+                                                PaymentService paymentService,
+                                                MakeAutoGenerateNumberService makeAutoGenerateNumberService,
+                                                EmailService emailService) {
+        this.vesselOrderService = vesselOrderService;
+        this.vesselOrderItemService = vesselOrderItemService;
+        this.vesselOrderItemBidService = vesselOrderItemBidService;
+        this.vesselOrderItemBidPaymentService = vesselOrderItemBidPaymentService;
+        this.paymentService = paymentService;
+        this.makeAutoGenerateNumberService = makeAutoGenerateNumberService;
+        this.emailService = emailService;
+    }
 
 
-    model.addAttribute("vesselOrderItemBids", vesselOrderItemBids);
-    model.addAttribute("vesselOrderDetail", vesselOrder);
-    model.addAttribute("vesselDetail", vesselOrder.getVesselArrivalHistory().getVessel());
-    List< BidValidOrNot > bidValidOrNots = new ArrayList<>();
-    bidValidOrNots.add(BidValidOrNot.REJECT);
-    bidValidOrNots.add(BidValidOrNot.SELECT);
-    model.addAttribute("bidValidOrNots", bidValidOrNots);
-    model.addAttribute("vesselOrderBid", new VesselOrderBid());
-    return "vesselOrderItemApproval/addVesselOrderItemApproval";
-  }
+    @GetMapping
+    public String findAll(Model model) {
+        model.addAttribute("vesselOrders", vesselOrderService.findByVesselOrderStatus(VesselOrderStatus.BIDEN));
+        return "vesselOrderItemApproval/vesselOrderItemApproval";
+    }
 
-  @PostMapping( "/save" )
-  public String saveApprove(@ModelAttribute VesselOrderBid vesselOrderBid) {
+    @GetMapping( "/{id}" )
+    public String addForm(@PathVariable Integer id, Model model) {
+        Comparator< VesselOrderItemBid > compareByBidenPrice =
+                Comparator.comparing(VesselOrderItemBid::getAmount);
 
-    List< VesselOrderItemBid > vesselOrderItemBids =
-        vesselOrderItemBidService.saveAll(vesselOrderBid.getVesselOrderItemBids());
+        List< VesselOrderItemBid > vesselOrderItemBids = new ArrayList<>();
 
-    for ( VesselOrder vesselOrder : vesselOrders(vesselOrderItemBids) ) {
-      for ( Chandler chandler : chandlers(vesselOrderItemBids) ) {
-        //make payment
-        Payment payment = new Payment();
-        //last payment
-        Payment lastPayment = paymentService.lastPayment();
-        if ( lastPayment == null ) {
-          payment.setCode("SLCP" + makeAutoGenerateNumberService.numberAutoGen(null).toString());
-        } else {
-          String previousCode = lastPayment.getCode().substring(4);
-          payment.setCode("SLCP" + makeAutoGenerateNumberService.numberAutoGen(previousCode).toString());
-        }
+        VesselOrder vesselOrder = vesselOrderService.findById(id);
 
-        List< BigDecimal > totalAmount = new ArrayList<>();
-        List< VesselOrderItemBid > vesselOrderItemBidsFiltered = vesselOrderItemBids
-            .stream()
-            .filter(x -> x.getBidValidOrNot().equals(BidValidOrNot.SELECT) && x.getChandler().equals(chandler) && x.getVesselOrderItem().getVesselOrder().equals(vesselOrder))
-            .collect(Collectors.toList());
-        vesselOrderItemBidsFiltered.forEach(x -> totalAmount.add(x.getAmount()));
-        //amount
-        BigDecimal totalPrice = totalAmount.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
-        payment.setAmount(totalPrice);
-        //statusConformation
-        payment.setStatusConformation(StatusConformation.INR);
-        //chandler
-        payment.setChandler(chandler);
-        //vesselOrder
-        payment.setVesselOrder(vesselOrder);
+        vesselOrder.getVesselOrderItems().forEach(
+                x -> vesselOrderItemBidService.findByVesselOrderItem(x)
+                        .stream()
+                        .sorted(compareByBidenPrice)
+                        .collect(Collectors.toList()).forEach(y -> {
+                            y.setItem(x.getItem());
+                            vesselOrderItemBids.add(y);
+                        }));
 
-        Payment paymentDb = paymentService.persist(payment);
-        vesselOrderItemBidsFiltered.forEach(x -> {
-          VesselOrderItemBidPayment vesselOrderItemBidPayment = new VesselOrderItemBidPayment();
-          vesselOrderItemBidPayment.setVesselOrderItemBid(x);
-          vesselOrderItemBidPayment.setPaymentStatus(PaymentStatus.ITEMNR);
-          vesselOrderItemBidPayment.setPayment(paymentDb);
-          vesselOrderItemBidPaymentService.persist(vesselOrderItemBidPayment);
-        });
 
-        //need to send email
+        model.addAttribute("vesselOrderItemBids", vesselOrderItemBids);
+        model.addAttribute("vesselOrderDetail", vesselOrder);
+        model.addAttribute("vesselDetail", vesselOrder.getVesselArrivalHistory().getVessel());
+        List< BidValidOrNot > bidValidOrNots = new ArrayList<>();
+        bidValidOrNots.add(BidValidOrNot.REJECT);
+        bidValidOrNots.add(BidValidOrNot.SELECT);
+        model.addAttribute("bidValidOrNots", bidValidOrNots);
+        model.addAttribute("vesselOrderBid", new VesselOrderBid());
+        return "vesselOrderItemApproval/addVesselOrderItemApproval";
+    }
 
-        StringBuilder message =
-            new StringBuilder(" Dear \n" + chandler.getName() + "\n According to this vessel order number  " + vesselOrder.getNumber() + " below item was approved \n\n\n\n" + "\t\t Item Name \t\t\t Quantity \t\t\t Unit Price(Rs.) \t\t\t Total");
+    @PostMapping( "/save" )
+    public String saveApprove(@ModelAttribute VesselOrderBid vesselOrderBid) {
 
-        for ( VesselOrderItemBid vesselOrderItemBid : vesselOrderItemBidsFiltered ) {
-          message.append("\t\t").append(vesselOrderItemBid.getVesselOrderItem().getItem().getName()).append("\t\t\t").append(vesselOrderItemBid.getVesselOrderItem().getQuantity()).append("\t\t\t").append(vesselOrderItemBid.getUnitPrice()).append("\t\t\t").append(vesselOrderItemBid.getAmount().toString());
-        }
-        if ( chandler.getEmail() != null ) {
-          emailService.sendEmail(chandler.getEmail(), "Inform Biden Vessel Order Status " + vesselOrder.getNumber(),
-                                 message.toString());
-        }
-      }
+        List< VesselOrderItemBid > vesselOrderItemBids =
+                vesselOrderItemBidService.saveAll(vesselOrderBid.getVesselOrderItemBids());
+
+        for ( VesselOrder vesselOrder : vesselOrders(vesselOrderItemBids) ) {
+            for ( Chandler chandler : chandlers(vesselOrderItemBids) ) {
+                //make payment
+                Payment payment = new Payment();
+                //last payment
+                Payment lastPayment = paymentService.lastPayment();
+                if ( lastPayment == null ) {
+                    payment.setCode("SLCP" + makeAutoGenerateNumberService.numberAutoGen(null).toString());
+                } else {
+                    String previousCode = lastPayment.getCode().substring(4);
+                    payment.setCode("SLCP" + makeAutoGenerateNumberService.numberAutoGen(previousCode).toString());
+                }
+
+                List< BigDecimal > totalAmount = new ArrayList<>();
+                List< VesselOrderItemBid > vesselOrderItemBidsFiltered = vesselOrderItemBids
+                        .stream()
+                        .filter(x -> x.getBidValidOrNot().equals(BidValidOrNot.SELECT) && x.getChandler().equals(chandler) && x.getVesselOrderItem().getVesselOrder().equals(vesselOrder))
+                        .collect(Collectors.toList());
+                vesselOrderItemBidsFiltered.forEach(x -> totalAmount.add(x.getAmount()));
+                //amount
+                BigDecimal totalPrice = totalAmount.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+                payment.setAmount(totalPrice);
+                //statusConformation
+                payment.setStatusConformation(StatusConformation.INR);
+                //chandler
+                payment.setChandler(chandler);
+                //vesselOrder
+                payment.setVesselOrder(vesselOrder);
+
+                Payment paymentDb = paymentService.persist(payment);
+                vesselOrderItemBidsFiltered.forEach(x -> {
+                    VesselOrderItemBidPayment vesselOrderItemBidPayment = new VesselOrderItemBidPayment();
+                    vesselOrderItemBidPayment.setVesselOrderItemBid(x);
+                    vesselOrderItemBidPayment.setPaymentStatus(PaymentStatus.ITEMNR);
+                    vesselOrderItemBidPayment.setPayment(paymentDb);
+                    vesselOrderItemBidPaymentService.persist(vesselOrderItemBidPayment);
+                });
+
+                //need to send email
+
+                StringBuilder message =
+                        new StringBuilder(" Dear \n" + chandler.getName() + "\n According to this vessel order number  " + vesselOrder.getNumber() + " below item was approved \n\n\n\n" + "\t\t Item Name \t\t\t Quantity \t\t\t Unit Price(Rs.) \t\t\t Total");
+
+                for ( VesselOrderItemBid vesselOrderItemBid : vesselOrderItemBidsFiltered ) {
+                    message.append("\t\t").append(vesselOrderItemBid.getVesselOrderItem().getItem().getName()).append("\t\t\t").append(vesselOrderItemBid.getVesselOrderItem().getQuantity()).append("\t\t\t").append(vesselOrderItemBid.getUnitPrice()).append("\t\t\t").append(vesselOrderItemBid.getAmount().toString());
+                }
+                if ( chandler.getEmail() != null ) {
+                    emailService.sendEmail(chandler.getEmail(), "Inform Biden Vessel Order Status " + vesselOrder.getNumber(),
+                            message.toString());
+                }
+            }
 //vesselOrder save after changing vesselOrder order status
-      vesselOrder.setVesselOrderStatus(VesselOrderStatus.APPROVE);
-      vesselOrderService.persist(vesselOrder);
+            vesselOrder.setVesselOrderStatus(VesselOrderStatus.APPROVE);
+            vesselOrderService.persist(vesselOrder);
+        }
+        return "redirect:/vesselOrderItemApproval";
     }
-    return "redirect:/vesselOrderItemApproval";
-  }
 
-  private List< Chandler > chandlers(List< VesselOrderItemBid > vesselOrderItemBids) {
-    List< Chandler > chandlers = new ArrayList<>();
-    vesselOrderItemBids.forEach(x -> chandlers.add(x.getChandler()));
-    return chandlers.stream().distinct().collect(Collectors.toList());
-  }
-
-  private List< VesselOrder > vesselOrders(List< VesselOrderItemBid > vesselOrderItemBids) {
-    List< VesselOrder > vesselOrders = new ArrayList<>();
-
-    for ( VesselOrderItemBid vesselOrderItemBid : vesselOrderItemBids ) {
-      vesselOrders.add(vesselOrderItemService.findById(vesselOrderItemBid.getVesselOrderItem().getId()).getVesselOrder());
+    private List< Chandler > chandlers(List< VesselOrderItemBid > vesselOrderItemBids) {
+        List< Chandler > chandlers = new ArrayList<>();
+        vesselOrderItemBids.forEach(x -> chandlers.add(x.getChandler()));
+        return chandlers.stream().distinct().collect(Collectors.toList());
     }
-    return vesselOrders.stream().distinct().collect(Collectors.toList());
-  }
+
+    private List< VesselOrder > vesselOrders(List< VesselOrderItemBid > vesselOrderItemBids) {
+        List< VesselOrder > vesselOrders = new ArrayList<>();
+
+        for ( VesselOrderItemBid vesselOrderItemBid : vesselOrderItemBids ) {
+            vesselOrders.add(vesselOrderItemService.findById(vesselOrderItemBid.getVesselOrderItem().getId()).getVesselOrder());
+        }
+        return vesselOrders.stream().distinct().collect(Collectors.toList());
+    }
 
 }
